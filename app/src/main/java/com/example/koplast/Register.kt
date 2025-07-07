9eb70461352db9f7f7f3bcfe351f66dde8395835
Filip Tadic <ftadic@tvz.hr>
2025-07-07 20:34:35 +0200

    Dizajniran Login i Register screen

--- app/src/main/java/com/example/koplast/Register.kt
+++ app/src/main/java/com/example/koplast/Register.kt
@@ -1,29 +1,22 @@
 package com.example.koplast
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnRegisterLogin = findViewById<Button>(R.id.btnRegisterLogin)
-
-        btnRegisterLogin.setOnClickListener {
-            val intent = Intent(this, Register::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_register)
     }
 }
